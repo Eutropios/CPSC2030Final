7d879b1d8bfc99ba6827ec2231593185399675e8
Gilard Villanueva <gilard99@gmail.com>
2025-03-28 20:13:57 -0700

    I changed teh card bootstrap and javascript to allow new notes to fit the format, and a small css for the card. Can change that later

--- views/js/cardFunctions.js
+++ views/js/cardFunctions.js
@@ -2,22 +2,28 @@
 
     document.addEventListener("DOMContentLoaded", function () {
         const newNote = document.querySelector(".btn-primary.btn-sm");
-      
+
         function createNoteCard() {
-            const cardColumn = document.querySelector(".col-9");
+            const cardColumn = document.querySelector(".d-flex.flex-wrap.gap-3");
             const card = document.createElement("div");
-            card.classList.add("card", "mt-3");
+            card.classList.add("card", "noteCard");
             card.innerHTML = `
-                <div class="card-header" contenteditable="true">New Note Title</div>
-                <div class="card-body" contenteditable="true">Edit note paragraph...</div>
-                <div class="card-footer">
-                    <div class="btn-group" role="group">
-                        <button class="btn btn-outline-primary">Edit</button>
-                        <button class="btn btn-outline-success">Share</button>
-                        <button class="btn btn-outline-danger">Delete</button>
-                    </div>
-                </div>
-            `;
+                        <div class="card-header" contenteditable="true">
+                            Header Name Here
+                        </div>
+                        <div class="card-body" contenteditable="true">
+                            Body paragraph here
+                            <hr>
+                            <span class="coords">Location will show here if added by user</span>
+                        </div>
+                        <div class="card-footer">
+                            <div class="btn-group" role="group">
+                                <button type="button" class="btn btn-outline-primary">Edit</button>
+                                <button type="button" class="btn btn-outline-success">Share</button>
+                                <button type="button" class="btn btn-outline-danger">Delete</button>
+                            </div>
+                        </div>
+                        `;
 
             card.querySelector(".btn-outline-danger").addEventListener("click", () => card.remove());
 
