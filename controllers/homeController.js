2e81ec18c51fb442885c4f25689832b4c4942816
Noah Jenner <64815328+Eutropios@users.noreply.github.com>
2025-03-31 17:24:13 -0700

    Multiple lints corrected. Removed semicolons
    Instructor dislikes semicolons, so I've removed them.
    
    Signed-off-by: <64815328+Eutropios@users.noreply.github.com>

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,27 +1,27 @@
-const util = require("../models/util.js");
-const config = require("../server/config/config");
-const User = require("../models/user");
-const client = util.getMongoClient();
+const util = require("../models/util.js")
+const config = require("../server/config/config")
+const User = require("../models/user")
+const client = util.getMongoClient()
 
-const express = require("express");
-const homeController = express.Router();
+const express = require("express")
+const homeController = express.Router()
 
 // HTTP POST
 homeController.post("/register", util.logRequest, async (req, res, next) => {
-    console.log("register");
-    let collection = client.db().collection("Users");
-    let email = req.body.email;
-    let password = req.body.password;
-    let confirm = req.body.confirm;
+    console.log("register")
+    const collection = client.db().collection("Users")
+    const email = req.body.email
+    const password = req.body.password
+    const confirm = req.body.confirm
 
     if (password !== confirm) {
-        console.log("\t|Password does not match");
+        console.log("\t|Password does not match")
     } else {
-        let user = User(email, password);
-        console.info(user);
-        util.insertOne(collection, user);
+        const user = User(email, password)
+        console.info(user)
+        util.insertOne(collection, user)
     }
-    res.redirect("/member.html");
-});
+    res.redirect("/member.html")
+})
 
-module.exports = homeController;
+module.exports = homeController
