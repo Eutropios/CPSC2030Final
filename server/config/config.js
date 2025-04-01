c44a02f878c6299458adbefa3b41a73ae204a8ee
Noah Jenner <64815328+Eutropios@users.noreply.github.com>
2025-03-31 17:34:04 -0700

    Added mongodb to packages
    Signed-off-by: <64815328+Eutropios@users.noreply.github.com>

--- server/config/config.js
+++ server/config/config.js
@@ -1,6 +1,6 @@
 ;(() => {
     const config = {}
-    config.PORT = process.env.PORT || 8081
+    config.PORT = process.env.PORT || 8080
     config.ROOT = "views"
     config.LOG_FILE = "server/log/node.js.log"
     module.exports = config
