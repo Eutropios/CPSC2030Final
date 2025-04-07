8ef40decd598e35397ec5b8cfc6c30eb098ce479
Trevor <tweiler00@mylangara.ca>
2025-04-07 00:28:50 -0700

    created partially functioning func for creating noteCards in index.hmtl from fetched Note data

--- views/js/cardFunctions.js
+++ views/js/cardFunctions.js
@@ -1,43 +1,43 @@
 (() => {
     document.addEventListener("DOMContentLoaded", () => {
         const newNote = document.querySelector("#addNote");
-
-        function createNoteCard() {
-            const cardColumn = document.querySelector("#cardContainer");
-            const card = document.createElement("div");
-            card.classList.add("card", "noteCard", "mt-2");
-            card.innerHTML = `
-                        <div class="card-header" contenteditable="true">
-                            Header Name Here
-                        </div>
-                        <div class="card-body">
-                           <p contenteditable="true">
-                            Body paragraph here
-                           </p>
-                           <hr>
-                            <span class="text-center coords">Location will show here if added by user</span>
-                        </div>
-                        </div>
-                        <div>
-                        <div class="card-footer">
-                            <div class="btn-group" role="group">
-                                <button type="button" class="btn btn-outline-primary">Edit</button>
-                                <button type="button" class="btn btn-outline-success">Share</button>
-                                <button type="button" class="btn btn-outline-danger">Delete</button>
-                            </div>
-                        </div>
-                        `;
-
-            card.querySelector(".btn-outline-danger").addEventListener("click", () =>
-                card.remove(),
-            );
-
-            cardColumn.appendChild(card);
-
-            // We'll have to add this to the database too afterwards
-            // Add listeners to note that modify database content for the note
-        }
-
         newNote.addEventListener("click", createNoteCard);
     });
+
+    const createNoteCard = (title, text) => {
+        const cardColumn = document.querySelector("#cardContainer");
+        const card = document.createElement("div");
+        card.classList.add("card", "noteCard", "mt-2");
+        card.innerHTML = `
+                    <div class="card-header" contenteditable="true">
+                        ${title}
+                    </div>
+                    <div class="card-body">
+                       <p contenteditable="true">
+                        ${text}
+                       </p>
+                       <hr>
+                        <span class="text-center coords">Location will show here if added by user</span>
+                    </div>
+                    </div>
+                    <div>
+                    <div class="card-footer">
+                        <div class="btn-group" role="group">
+                            <button type="button" class="btn btn-outline-primary">Edit</button>
+                            <button type="button" class="btn btn-outline-success">Share</button>
+                            <button type="button" class="btn btn-outline-danger">Delete</button>
+                        </div>
+                    </div>
+                    `;
+
+        card.querySelector(".btn-outline-danger").addEventListener("click", () =>
+            card.remove(),
+        );
+
+        cardColumn.appendChild(card);
+
+        // We'll have to add this to the database too afterwards
+        // Add listeners to note that modify database content for the note
+    };
+    module.exports = createNoteCard;
 })();
