b3743902d5e928acc5f765d9cf3ceb8ff97888ba
Trevor <tweiler00@mylangara.ca>
2025-04-09 22:13:21 -0700

    added to template html for noteCards in cardFunctions.js to include a modal for editing a note

--- views/js/cardFunctions.js
+++ views/js/cardFunctions.js
@@ -1,41 +1,47 @@
+const { default: test } = require("node:test");
+
 (() => {
     const createNoteCard = (title, text, date) => {
         const cardColumn = document.querySelector("#cardContainer");
         const card = document.createElement("div");
         card.classList.add("card", "noteCard", "mt-2");
         card.innerHTML = `
-                    <div class="card-header" contenteditable="true">
-                        ${title}
-                    </div>
-                    <div class="card-body">
-                       <p contenteditable="true">
+                <div class="card-header" contenteditable="true">${title}</div>
+                <div class="card-body">
+                    <p contenteditable="true">
                         ${text}
-                       </p>
-                       <hr>
-                        <button type="button" class="btn btn-success btn-sm" data-bs-toggle="modal" data-bs-target="#${date.replace(" ", "-")}">
-                            Sign Up
-                        </button>
-                        <div
-                        class="modal fade"
-                        id="${date.replace(" ", "-")}"
-                        tabindex="-1"
-                        role="dialog"
-                        aria-labelledby="${date.replace(" ", "-")}"
-                        aria-hidden="true">
-                            <div class="modal-dialog modal-dialog-centered" role="document">
-                                <div class="modal-content">
-                                <div>Mapbox API content will go here, API call will be sent server-side and sent back here</div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    </div>
-                    <div>
+                    </p>
+                    <hr>
+                    <span>Button for opening mapbox goes here</span>
+                </div>
+                <div>
                     <div class="card-footer">
                         <div class="btn-group" role="group">
-                            <button type="button" class="btn btn-outline-primary">Edit</button>
+                            <button type="button" class="btn btn-outline-primary" id="btn-${test}" data-bs-toggle="modal" data-bs-target="${test}">Edit</button>
                             <button type="button" class="btn btn-outline-success">Share</button>
                             <button type="button" class="btn btn-outline-danger">Delete</button>
+                        </div>
+                        <div class="modal fade" id="${test}" tabindex="-1" role="dialog" aria-labelledby="${test}" aria-hidden="true">
+                            <div class="modal-dialog modal-dialog-centered" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title">EDIT</h5>
+                                    </div>
+                                    <div class=modal-body: id="edit-${test}">
+                                        <form id="editform-${test}">
+                                            <div class="form-group">
+                                                <label for="update-text" class="col-form-label">New Text
+                                                <textarea name="update-text" rows="8" cols="60"></textarea>
+                                            </div>
+                                            <hr>
+                                            <div>
+                                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</btn>
+                                                <button type="submit" value="submit" class="btn btn-success">Submit</btn>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                         <span>${date}</span>
                     </div>
